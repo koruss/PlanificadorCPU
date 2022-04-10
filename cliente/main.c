458b29d19a15a959615d7231338c91c569486fcb
lombax <anner.calvo@gmail.com>
2022-04-10 17:53:58 -0600

    arreglando la conexión en el socket

--- cliente/main.c
+++ cliente/main.c
@@ -8,7 +8,8 @@
 
 typedef struct
 {
-    int i;
+    int i, argc;
+    char **argv;
 } connection_t;
 
 
@@ -24,71 +25,73 @@ void * processClient(void * ptr)
     struct hostent * host;
     int len;
 
-//    /* checking commandline parameter */
-//    if (argc != 4)
-//    {
-//        printf("usage: %s hostname port text\n", argv[0]);
-//    }
+    /* checking commandline parameter */
+    if (conn->argc != 4)
+    {
+        printf("usage: %s hostname port text\n", conn->argv[0]);
+    }
 
-//    /* obtain port number */
-//    if (sscanf(argv[2], "%d", &port) <= 0)
-//    {
-//        fprintf(stderr, "%s: error: wrong parameter: port\n", argv[0]);
-//    }
+    /* obtain port number */
+    if (sscanf(conn->argv[2], "%d", &port) <= 0)
+    {
+        fprintf(stderr, "%s: error: wrong parameter: port\n", conn->argv[0]);
+    }
 
     /* create socket */
     sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-//    if (sock <= 0)
-//    {
-//        fprintf(stderr, "%s: error: wrong parameter: port\n", argv[0]);
-//    }
+    if (sock <= 0)
+    {
+        fprintf(stderr, "%s: error: cannot create socket\n", conn->argv[0]);
+    }
 
     /* connect to server */
     address.sin_family = AF_INET;
-    address.sin_port = htons(8980);
-    host = gethostbyname("localhost");
+    address.sin_port = htons(port);
+    host = gethostbyname(conn->argv[1]);
+    if (!host)
+    {
+        fprintf(stderr, "%s: error: unknown host %s\n", conn->argv[0], conn->argv[1]);
 
-//    if (!host)
-//    {
-//        fprintf(stderr, "%s: error: wrong parameter: port\n", argv[0]);
-//    }
+    }
     memcpy(&address.sin_addr, host->h_addr_list[0], host->h_length);
-//    if (connect(sock, (struct sockaddr *)&address, sizeof(address)))
-//    {
-//        fprintf(stderr, "%s: error: wrong parameter: port\n", argv[0]);
-//    }
+    if (connect(sock, (struct sockaddr *)&address, sizeof(address)))
+    {
+        fprintf(stderr, "%s: error: cannot connect to host %s\n", conn->argv[0], conn->argv[1]);
+
+    }
 
     /* send text to server */
-    len = strlen("argv[3]");
     printf("%d\n", conn->i);
+    len = strlen(conn->argv[3]);
     write(sock, &len, sizeof(int));
-    write(sock, "argv[3]", len);
+    write(sock, conn->argv[3], len);
 
     /* close socket */
     close(sock);
 
-    return 0;
+
 }
 
 void r(int i){
     printf("%d\n", i);
 }
 
-int main()
+int main(int argc, char **argv)
 {
     connection_t * connection;
     pthread_t thread;
 
 
     connection = (connection_t *)malloc(sizeof(connection_t));
-//    connection->argv = argv;
     int i=0;
     connection->i = i;
+    connection->argv = argv;
+    connection->argc = argc;
     while(1){
         pthread_create(&thread, 0, processClient, (void *)connection);
         pthread_detach(thread);
 
-        sleep(1);
+        sleep(5);
         ++i;
     }
     pthread_exit(0);
