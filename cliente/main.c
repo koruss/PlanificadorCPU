7f24a796054f833c1ba42ace727f71e77be0d031
lombax <anner.calvo@gmail.com>
2022-04-11 00:40:03 -0600

    Limpiando cliente

--- cliente/main.c
+++ cliente/main.c
@@ -7,12 +7,9 @@
 #include <pthread.h>
 #include <sys/queue.h>
 
-typedef struct connection_t
-{
-    int i, argc;
-    char **argv;
-} connection_t;
-
+// Global
+char *HOST_NAME = "localhost";
+int PORT = 8089;
 
 typedef struct _Node {
     LIST_ENTRY(_Node) pointers;
@@ -32,49 +29,35 @@ int MODE=1;//the client mode 1 equals to MANUAL mode 2 equals to AUTO mode
 
 void * processClient(void * param)
 {
-    connection_t * conn;
-
-    int port;
     int sock = -1;
     struct sockaddr_in address;
-    char *host_name = "localhost";
-    struct hostent * host = gethostbyname(host_name);
+    struct hostent * host = gethostbyname(HOST_NAME);
     int len;
-
-
 
     /* create socket */
     sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
     /* connect to server */
     address.sin_family = AF_INET;
-    address.sin_port = htons(8980);
+    address.sin_port = htons(PORT);
     if (!host)
     {
-        fprintf(stderr, "%s: error: unknown host %s\n", conn->argv[0], conn->argv[1]);
-
+        fprintf(stderr, "%s: error: unknown host %s\n", "Client", HOST_NAME);
     }
-
 
     memcpy(&address.sin_addr, host->h_addr_list[0], host->h_length);
     if (connect(sock, (struct sockaddr *)&address, sizeof(address)))
     {
-        fprintf(stderr, "%s: error: cannot connect to host %s\n", conn->argv[0], conn->argv[1]);
-
+        fprintf(stderr, "%s: error: cannot connect to host %s\n", "Client", HOST_NAME);
     }
 
     /* send text to server */
-//    len = strlen("conn->argv[3]");
-//    write(sock, &len, sizeof(int));
-//    write(sock, "conn->argv[3]", len);
     len = strlen(param);
     write(sock, &len, sizeof(int));
     write(sock, param, len);
 
     /* close socket */
-    close(sock);
-
-
+//    close(sock);
 }
 
 void* sendToServer(void *arg){
@@ -83,44 +66,6 @@ void* sendToServer(void *arg){
     processClient(arg);
     return NULL;
 }
-
-
-//void* manual(void * arg){
-//    char fileName[]= "/home/koruss/Documents/Bretes/PlanificadorCPU/cliente/manual.txt";
-//    FILE *fp = fopen(fileName, "r");
-//    const char s[2] = ","; ///importante para aplicar en el server
-//    char *token;
-//    int i = 0;
-//    struct process p1;
-//    int ch = 6; //the amount of threads
-//    if(fp != NULL)
-//    {
-//        threadList=(pthread_t *)malloc(ch * sizeof(pthread_t ));
-//        char line[20];///importante para aplicar en el server
-//        while(fgets(line, sizeof line, fp) != NULL)// read line by line
-//        {
-//            p1.burst = atoi(strtok(line, s));//read the burst part and save it in the struct
-//            token = strtok(NULL,s);//read the second part of the line
-//            p1.priority = atoi(token);//save it in the struct
-//            p1.id = i;
-//            //printf("Burst = %d\t, Priority = %d \n", p1.burst, p1.priority);
-//            pthread_create(&threadList[i],NULL, sendToServer,(void *)&p1);
-//            i = i+1;
-//            //here we should call the socket emit fuction
-//            int value = (rand()%6) + 3;
-//            sleep( value ); //a number between 3 to 8
-//        }
-//        for(i=0;i<ch;i++)
-//        {
-//            pthread_join(threadList[i],NULL);
-//        }
-//        fclose(fp);//close the document
-//    }
-//    else {
-//        printf("%s", "An Error Ocurred Reading the file" );
-//    }
-//    return NULL;
-//}
 
 
 void manual(char **lista, int threadsAmount){
@@ -194,21 +139,15 @@ void autoMode(int min, int max,int ratio){
         char priorityStr[sizeof(priority)];
         sprintf(priorityStr, "%d", priority);// converts the priority to a string
 
-//        printf("%s", "priority: ");
-//        printf("%s", priorityStr);
-
         int burst = ( rand() % (max-min+1) )+ min;//calculates the burst based on min and max given
         char burstStr[sizeof(burst)];
         sprintf(burstStr , "%d" , burst);
-//        printf("%s\n","burst ");
-//        printf("%s\n", burstStr);
         const char s[2] = ",";
 
         char line[20]="";
         strcat(line , burstStr);
         strcat(line,s);
         strcat(line,priorityStr);
-
 
         pthread_t thread;
         _Node *node = createNode(thread);
@@ -223,16 +162,6 @@ void autoMode(int min, int max,int ratio){
 
 int main(int argc, char **argv)
 {
-    connection_t * connection;
-    pthread_t thread;
-
-
-    connection = (connection_t *)malloc(sizeof(connection_t));
-    int i=0;
-    connection->i = i;
-    connection->argv = argv;
-    connection->argc = argc;
-
     printf("SELECT THE MODE (TYPE THE NUMBER): \n\t1.MANUAL\n\t2.AUTOMATIC\n");
     scanf("%d",&MODE);
     if(MODE == 1){
@@ -253,15 +182,11 @@ int main(int argc, char **argv)
         scanf("%d",&ratio);
         autoMode(min, max,ratio);
     }
-
-
-
     else{
         //retry
         printf("SELECT A VALID MODE");
     }
     pthread_exit(0);
-
 
     return 0;
 }
